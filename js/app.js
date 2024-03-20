723feeec8cc902df4b9ee47e8417351acf340f4c
alexL <aleksandrlambert@gmail.com>
2024-03-20 19:00:47 +0700

    проверка на ошибки

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 async function getData() {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json()
-    return data
+    const data = await response.json();
+    return data;
 }
 
 async function main() {
-    const postData = await getData();
+    const postsData = await getData();
     let currentPage = 1;
     let rows = 10;
 
     function displayList(arrData, rowPerPage, page) {
         const postsEl = document.querySelector('.posts');
-        postEl.innerHTML = '';
+        postsEl.innerHTML = "";
         page--;
 
         const start = rowPerPage * page;
@@ -24,39 +24,43 @@ async function main() {
             postEl.innerText = `${el.title}`;
             postsEl.appendChild(postEl);
         })
-
     }
+
     function displayPagination(arrData, rowPerPage) {
         const paginationEl = document.querySelector('.pagination');
         const pagesCount = Math.ceil(arrData.length / rowPerPage);
         const ulEl = document.createElement("ul");
-        ulEl.classList.add("pagination__list");
+        ulEl.classList.add('pagination__list');
 
         for (let i = 0; i < pagesCount; i++) {
             const liEl = displayPaginationBtn(i + 1);
-            ulEl.appendChild(liEl);
+            ulEl.appendChild(liEl)
         }
-        paginationEl.appendChild(ulEl);
+        paginationEl.appendChild(ulEl)
     }
-    function displayPaginationBtn() {
-        const liEl = document.createElement('li');
-        liEl.classList.add('pagination__item');
-        liEl.innerText = page;
+
+    function displayPaginationBtn(page) {
+        const liEl = document.createElement("li");
+        liEl.classList.add('pagination__item')
+        liEl.innerText = page
 
         if (currentPage == page) liEl.classList.add('pagination__item--active');
 
         liEl.addEventListener('click', () => {
-            currentPage = page;
-            displayList(postData, rows, currentPage);
+            currentPage = page
+            displayList(postsData, rows, currentPage)
 
-            const listItems = document.querySelectorAll('.pagination__item');
-            currentItemLi.classList.remuve('pagination__item--active');
+            let currentItemLi = document.querySelector('li.pagination__item--active');
+            currentItemLi.classList.remove('pagination__item--active');
+
+            liEl.classList.add('pagination__item--active');
         })
+
         return liEl;
     }
 
-    displayList(postData, rows, currentPage);
-    displayPagination(postData, rows);
+    displayList(postsData, rows, currentPage);
+    displayPagination(postsData, rows);
 }
 
 main();
